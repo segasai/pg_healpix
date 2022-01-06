93220a4588974d9942d5bca7cf1e58048a441a04
Sergey E. Koposov <skoposov@ed.ac.uk>
2022-01-06 18:40:19 +0000

    make functions parallel safe

--- pg_healpix--1.0.sql
+++ pg_healpix--1.0.sql
@@ -6,7 +6,7 @@
 CREATE FUNCTION healpix_ang2ipix_nest(bigint, double precision, double precision)
 RETURNS bigint
 AS 'MODULE_PATHNAME', 'pgheal_ang2ipix_nest'
-LANGUAGE C IMMUTABLE STRICT;
+LANGUAGE C IMMUTABLE PARALLEL SAFE STRICT;
 
 COMMENT ON FUNCTION healpix_ang2ipix_nest (bigint, double precision, double precision)
 IS 'Function converting nside, Ra and Dec to the healpix nested ipix value';
@@ -14,7 +14,7 @@ IS 'Function converting nside, Ra and Dec to the healpix nested ipix value';
 CREATE OR REPLACE FUNCTION healpix_ang2ipix_ring(bigint, double precision, double precision)
 RETURNS bigint
 AS 'MODULE_PATHNAME', 'pgheal_ang2ipix_ring'
-LANGUAGE C IMMUTABLE STRICT;
+LANGUAGE C IMMUTABLE PARALLEL SAFE STRICT;
 
 COMMENT ON FUNCTION healpix_ang2ipix_ring (bigint, double precision, double precision)
 IS 'Function converting nside, Ra and Dec to the healpix ring ipix value';
@@ -22,7 +22,7 @@ IS 'Function converting nside, Ra and Dec to the healpix ring ipix value';
 CREATE OR REPLACE FUNCTION healpix_ipix2ang_nest(nside bigint, ipix bigint)
 RETURNS double precision[]
 AS 'MODULE_PATHNAME', 'pgheal_ipix2ang_nest'
-LANGUAGE C IMMUTABLE STRICT;
+LANGUAGE C IMMUTABLE PARALLEL SAFE STRICT;
 
 COMMENT ON FUNCTION healpix_ipix2ang_nest(bigint, bigint)
 IS 'Function converting the Healpix nested ipix value to Ra, Dec';
@@ -30,7 +30,7 @@ IS 'Function converting the Healpix nested ipix value to Ra, Dec';
 CREATE OR REPLACE FUNCTION healpix_ipix2ang_ring(nside bigint, ipix bigint)
 RETURNS double precision[]
 AS 'MODULE_PATHNAME', 'pgheal_ipix2ang_ring'
-LANGUAGE C IMMUTABLE STRICT;
+LANGUAGE C IMMUTABLE PARALLEL SAFE STRICT;
 
 COMMENT ON FUNCTION healpix_ipix2ang_ring(bigint, bigint)
 IS 'Function converting the Healpix ring ipix value to Ra, Dec';
